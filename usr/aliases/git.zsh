53e4cd3fd121d418821439cbccfb70e770923f50
Skril <klodiys62@gmail.com>
2022-11-03 17:27:12 +0300

    make ssh-add alias correct on non macOS systems

--- usr/aliases/git.zsh
+++ usr/aliases/git.zsh
@@ -1,3 +1,10 @@
 alias gf="git add .; git commit; git push"
-alias gi="ssh-add --apple-use-keychain  '$HOME/.ssh/github'; ssh-add --apple-use-keychain '$HOME/.ssh/gitlab'"
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+  alias gi="ssh-add --apple-use-keychain  '$HOME/.ssh/github'; ssh-add --apple-use-keychain '$HOME/.ssh/gitlab'"
+else
+  alias gi="ssh-add '$HOME/.ssh/github'; ssh-add '$HOME/.ssh/gitlab'"
+fi
+
 alias gssh="eval 'ssh-agent -s'; ssh-add ~/.ssh/id_ed25519"
