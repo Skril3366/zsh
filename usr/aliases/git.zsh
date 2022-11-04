a2b75309457f16a73a868eeef00e12dedaf89ee5
a.ragulin <a.ragulin@macbook-a.ragulin>
2022-11-04 10:04:58 +0300

    add alias to update all my git repositories

--- usr/aliases/git.zsh
+++ usr/aliases/git.zsh
@@ -1,10 +1,32 @@
 alias gf="git add .; git commit; git push"
-
-if [[ "$OSTYPE" == "darwin"* ]]
-then
-  alias gi="ssh-add --apple-use-keychain  '$HOME/.ssh/github'; ssh-add --apple-use-keychain '$HOME/.ssh/gitlab'"
-else
-  alias gi="ssh-add '$HOME/.ssh/github'; ssh-add '$HOME/.ssh/gitlab'"
-fi
-
+alias gi="ssh-add --apple-use-keychain  '$HOME/.ssh/github'; ssh-add --apple-use-keychain '$HOME/.ssh/gitlab'"
 alias gssh="eval 'ssh-agent -s'; ssh-add ~/.ssh/id_ed25519"
+
+function gup {
+  for c in "${configs[@]}"; do
+    echo "Updating $c"
+    pull_rebase "$HOME/$c"
+  done
+}
+
+configs=(
+  "/.config/nvim"
+  "/.config/alacritty"
+  "/.config/zsh"
+  "/.config/yabai"
+  "/.config/skhd"
+  )
+
+
+
+function pull_rebase {
+  DIR="$(pwd)"
+  cd "$1"
+  git add .
+  git stash
+  git pull --rebase
+  git stash pop
+  git submodule update --recursive
+  git reset
+  cd "$DIR"
+}
